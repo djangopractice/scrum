ef51080fae5465de48996e91f5577bb1ec500648
Dave Koopman <dkoopman@godaddy.com>
2014-12-25 22:54:17 -0700

    api first working

---
+++ board/serializers.py
@@ -0,0 +1,67 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework.reverse import reverse
+from .models import Sprint, Task
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    full_name = serializers.CharField(source='get_full_name', read_only=True)
+    links = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active', 'links', )
+
+    def get_links(self, obj):
+        request = self.context['request']
+        username = obj.get_username()
+        return {
+            'self': reverse('user-detail', kwargs={User.USERNAME_FIELD: username}, request=request),
+        }
+
+
+class SprintSerializer(serializers.ModelSerializer):
+
+    links = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Sprint
+        fields = ('id', 'name', 'description', 'end', 'links', )
+
+    def get_links(self, obj):
+        request = self.context['request']
+        return {'self': reverse('sprint-detail', kwargs={'pk': obj.ok}, request=request), }
+
+
+class TaskSerializer(serializers.ModelSerializer):
+
+    assigned = serializers.SlugRelatedField(slug_field=User.USERNAME_FIELD, required=False,
+                                            read_only=True)
+    status_display = serializers.SerializerMethodField('get_status_display')
+    links = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Task
+        fields = ('id', 'name', 'description', 'sprint', 'status', 'order', 'assigned', 'started',
+                  'due', 'completed', 'links', )
+
+    def get_status_display(self, obj):
+        return obj.get_status_display()
+
+    def get_links(self, obj):
+        request = self.context['request']
+        links = {
+            'self': reverse('task-detail', kwargs={'pk': obj.ok}, request=request),
+            'sprint': None,
+            'assigned': None
+        }
+        if obj.sprint_id:
+            links['sprint'] = reverse('sprint-detail', kwargs={'pk': obj.sprint_id},
+                                      request=request)
+        if obj.assigned:
+            links['assigned'] = reverse('user-detail', kwargs={User.USERNAME_FIELD: obj.assigned},
+                                        request=request)
+        return links
--- scrum/urls.py
+++ scrum/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from rest_framework.authtoken.views import obtain_auth_token
+from board.urls import router
 
 urlpatterns = (
     url(r'^api/token/', obtain_auth_token, name='api-token'),
+    url(r'^api/v1/', include(router.urls)),
 )
