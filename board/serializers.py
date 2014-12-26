acb7c448905c72faaa8f1a07068e6e7bc5a9332e
Dave Koopman <dkoopman@godaddy.com>
2014-12-26 00:39:31 -0700

    Added links to filtered tasks in sprint-list and user-list

--- board/serializers.py
+++ board/serializers.py
@@ -20,6 +20,7 @@ class UserSerializer(serializers.ModelSerializer):
         username = obj.get_username()
         return {
             'self': reverse('user-detail', kwargs={User.USERNAME_FIELD: username}, request=request),
+            'tasks': '{}?assigned={}'.format(reverse('task-list', request=request), username),
         }
 
 
@@ -33,7 +34,10 @@ class SprintSerializer(serializers.ModelSerializer):
 
     def get_links(self, obj):
         request = self.context['request']
-        return {'self': reverse('sprint-detail', kwargs={'pk': obj.pk}, request=request), }
+        return {
+            'self': reverse('sprint-detail', kwargs={'pk': obj.pk}, request=request),
+            'tasks': reverse('task-list', request=request) + '?sprint={}'.format(obj.pk),
+        }
 
 
 class TaskSerializer(serializers.ModelSerializer):
